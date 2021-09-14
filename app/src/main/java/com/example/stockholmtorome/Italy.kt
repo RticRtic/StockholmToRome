2e43e088f74f4106d8d28e15cc232f4d5c64c06b
mansah82 <manne.sahlin@iths.se>
2021-09-14 15:31:51 +0200

    Changed comment

--- app/src/main/java/com/example/stockholmtorome/Italy.kt
+++ app/src/main/java/com/example/stockholmtorome/Italy.kt
@@ -37,7 +37,7 @@ class Italy : AppCompatActivity() {
         button.setOnClickListener {
             userAnswerItaly()
         }
-            // manne was here
+            // manne wa here
 
 
 
