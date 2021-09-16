0e02bb3b2eb20211f8094e2ccdbd67db66b3c6f1
Jesper Söderling <jesper.soderling@me.com>
2021-09-16 15:04:35 +0200

    Hey

---
+++ app/src/main/java/com/example/stockholmtorome/TrainGif.kt
@@ -0,0 +1,11 @@
+package com.example.stockholmtorome
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TrainGif : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_train_gif)
+    }
+}
